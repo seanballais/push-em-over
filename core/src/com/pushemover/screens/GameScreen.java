c70723b0fa94fb7a23f0a75d22298541100c5469
seanballais <sean@seanballais.com>
2016-10-02 20:39:27 +0800

    Started from scratch again.

--- core/src/com/pushemover/screens/GameScreen.java
+++ core/src/com/pushemover/screens/GameScreen.java
@@ -12,21 +12,12 @@ import java.util.Random;
 public class GameScreen extends AbstractScreen
 {
     private Stage game_stage;
-    private Platform[] platforms;
-    private boolean started;
 
     public GameScreen ( Game game )
     {
         super(game);
 
         game_stage = new Stage ();
-        started = false;
-
-        platforms = new Platform [ 10 ];
-        for ( int ctr = 0; ctr < platforms.length; ctr++ ) {
-            platforms [ ctr ] = new Platform ();
-            game_stage.addActor ( platforms[ ctr ] );
-        }
     }
 
     @Override public void dispose ()
@@ -40,8 +31,6 @@ public class GameScreen extends AbstractScreen
 
         Gdx.gl.glClearColor ( 111/255f, 169/255f, 235/255f, 1 );
         Gdx.gl.glClear ( GL20.GL_COLOR_BUFFER_BIT );
-
-        handlePlatformPositions ();
 
         game_stage.act ( delta );
         game_stage.draw ();
@@ -59,46 +48,5 @@ public class GameScreen extends AbstractScreen
     @Override public void hide ()
     {
         Gdx.app.debug ( "Game", "Dispose game screen" );
-    }
-
-    private void handlePlatformPositions ()
-    {
-        Random rand = new Random ();
-        int col_width = Gdx.graphics.getWidth () / platforms.length;
-        int row_height = Gdx.graphics.getHeight () / ( platforms.length + 7 );
-        boolean [] used_row = new boolean [ platforms.length + 5 ];
-        for ( int ctr = 0; ctr < platforms.length / 2; ctr++ ) {
-            if (!started) {
-                platforms[ctr].setXPos( ( col_width + platforms [ ctr ].getTextureWidth () ) * ctr );
-                platforms[ctr].setXPos( ( col_width + platforms [ ctr ].getTextureWidth () ) * ctr );
-
-                int row = rand.nextInt ( platforms.length + 4 );
-                while ( used_row [ row ] ) {
-                    row = rand.nextInt ( platforms.length + 4 );
-                }
-                used_row[row] = true;
-
-                platforms [ ctr ].setYPos ( row * row_height );
-                platforms [ ctr + 5 ].setYPos ( ( row * row_height ) * 2 );
-            } else {
-                if ( platforms [ ctr ].getYPos () < ( -1 * ( platforms [ ctr ].getTextureHeight () ) ) ) {
-                    platforms [ ctr ].setYPos ( Gdx.graphics.getHeight () + 100 );
-                }
-
-                if ( platforms [ ctr + 5 ].getYPos () < ( -1 * ( platforms [ ctr + 5 ].getTextureHeight () ) ) ) {
-                    platforms [ ctr + 5 ].setYPos ( Gdx.graphics.getHeight () + 300 );
-                }
-            }
-        }
-
-        if ( !started ) {
-            for ( int ctr = 0; ctr < platforms.length / 2; ctr++ ) {
-                int newYDelta = rand.nextInt ( 4 ) + 1;
-                platforms [ ctr ].setYDelta ( newYDelta );
-                platforms [ ctr + 5 ].setYDelta ( newYDelta );
-            }
-        }
-
-        started = true;
     }
 }
