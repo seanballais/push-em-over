fa9e3f6a2f83b98d49edcb6630e6fa76ac458e88
seanballais <sean@seanballais.com>
2016-10-06 16:04:36 +0800

    Added additional vector operations.

--- core/src/com/pushemover/util/VectorOperations.java
+++ core/src/com/pushemover/util/VectorOperations.java
@@ -4,24 +4,34 @@ import java.awt.Point;
 
 public class VectorOperations
 {
-    public Point add ( Point p1, Point p2 )
+    public static Point add ( Point p1, Point p2 )
     {
         return new Point ( ( p1.x + p2.x ), ( p1.y + p2.y ) );
     }
 
-    public Point subtract ( Point p1, Point p2 )
+    public static Point subtract ( Point p1, Point p2 )
     {
         return new Point ( ( p1.x - p2.x ), ( p1.y - p2.y ) );
     }
 
-    public int vectorDistanceBetween ( Point p1, Point p2 ) {
+    public static Point normalize ( Point p )
+    {
+        return new Point ( ( p.x / magnitude ( p ) ), ( p.y / magnitude ( p ) ) );
+    }
+
+    public static double magnitude ( Point p )
+    {
+        return Math.hypot ( p.x, p.y );
+    }
+
+    public static double distanceBetween ( Point p1, Point p2 ) {
         int xDistance = componentDifference ( p1.x, p2.x );
         int yDistance = componentDifference ( p1.y, p2.y );
 
-        return ( int ) Math.sqrt( xDistance + yDistance );
+        return Math.hypot ( xDistance, yDistance );
     }
 
-    public int componentDifference ( int c1, int c2 ) {
+    private static int componentDifference ( int c1, int c2 ) {
         return Math.max ( c1, c2 ) - Math.min ( c1, c2 );
     }
 }
