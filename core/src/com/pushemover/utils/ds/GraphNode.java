510961cfb3d9b73fb6fad0fecacbac1d053a22a7
seanballais <sean@seanballais.com>
2016-10-14 16:42:45 +0800

    Added a Graph node.

---
+++ core/src/com/pushemover/utils/ds/GraphNode.java
@@ -0,0 +1,47 @@
+package com.pushemover.utils.ds;
+
+import java.util.ArrayList;
+
+public class GraphNode
+{
+    private ArrayList <GraphNode> neighbours;
+    public boolean selected;
+    public int x;
+    public int y;
+
+    public GraphNode ( int x, int y )
+    {
+        neighbours = new ArrayList <GraphNode> ();
+        selected = false;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void addNeighbour ( GraphNode node )
+    {
+        neighbours.add ( node );
+    }
+
+    public void removeNeighbour ( GraphNode node )
+    {
+        neighbours.remove ( node );
+    }
+
+    public void removeNeighbourWithIndex ( int index )
+    {
+        neighbours.remove ( index );
+    }
+
+    public GraphNode getNeighbourWithIndex ( int index )
+    {
+        return neighbours.get( index );
+    }
+
+    public double distanceToNeighbour ( GraphNode node )
+    {
+        int xDist = Math.abs ( x - node.x );
+        int yDist = Math.abs ( y - node.y );
+
+        return Math.sqrt ( Math.pow( xDist, 2 ) + Math.pow ( yDist, 2 ) );
+    }
+}
