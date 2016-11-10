98ff925d2952510aa035b4461d5aab1e6901941f
seanballais <sean@seanballais.com>
2016-11-10 22:32:46 +0800

    Removed unnecessary import in GameScreen.

--- core/src/com/pushemover/screens/GameScreen.java
+++ core/src/com/pushemover/screens/GameScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.TimeUtils;
 import com.pushemover.actors.Platform;
 import com.pushemover.actors.Player;
 import com.pushemover.enums.ScreenEnum;
