3d60ed6788f01b6e8ecbbfeeaffe0cca79632c30
seanballais <sean@seanballais.com>
2016-11-10 22:32:27 +0800

    Removed Box2D code in GameScreen.

--- core/src/com/pushemover/screens/GameScreen.java
+++ core/src/com/pushemover/screens/GameScreen.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.pushemover.actors.Platform;
@@ -15,20 +13,16 @@ import com.pushemover.enums.ScreenEnum;
 import com.pushemover.handlers.PlatformHandler;
 import com.pushemover.handlers.ScreenHandler;
 import com.pushemover.preferences.GamePreferences;
-import com.pushemover.utils.Physics;
 
 import java.util.ArrayList;
 
 public class GameScreen extends AbstractScreen
 {
-    private World gameWorld;
     private OrthographicCamera camera;
     private Stage game_stage;
-    private ArrayList < Platform > platforms;
     private PlatformHandler pHandler;
     private Player player;
     private double accumulator;
-    private double currentTime;
     private float step;
 
     public GameScreen ( Game game )
@@ -37,10 +31,9 @@ public class GameScreen extends AbstractScreen
 
         step = 1.0f / 60f;
 
-        gameWorld = new World ( new Vector2 ( 0, -9.8f ), true );
+        GamePreferences gprefs = new GamePreferences ();
         pHandler = new PlatformHandler ();
-        player = new Player( gameWorld );
-        gameWorld.setContactListener ( new CollisionDetectionHandler( player, pHandler ) );
+        player = new Player( gprefs.getWidthResolution () / 2, gprefs.getHeightResolution () );
     }
 
     @Override public void dispose ()
@@ -54,18 +47,6 @@ public class GameScreen extends AbstractScreen
             ScreenHandler.getInstance().showScreen ( ScreenEnum.MAIN_MENU, game );
         }
 
-        if ( Gdx.input.isKeyPressed ( Input.Keys.W ) && player.isOnGround () ) { // TODO: Improve this part.
-            player.getBody ().applyLinearImpulse ( new Vector2 ( 0, 161f ), player.getBody ().getPosition (), true );
-        }
-
-        if ( Gdx.input.isKeyPressed ( Input.Keys.A ) ) {
-            player.getBody ().applyLinearImpulse ( new Vector2 ( -9.8f, 0 ), player.getBody ().getPosition (), true );
-        }
-
-        if ( Gdx.input.isKeyPressed ( Input.Keys.D ) ) {
-            player.getBody ().applyLinearImpulse ( new Vector2 ( 9.8f, 0 ), player.getBody ().getPosition (), true );
-        }
-
         delta = Math.min ( 0.06f, Gdx.graphics.getDeltaTime () );
 
         camera.update ();
@@ -73,13 +54,13 @@ public class GameScreen extends AbstractScreen
         Gdx.gl.glClearColor ( 111/255f, 169/255f, 235/255f, 1 );
         Gdx.gl.glClear ( GL20.GL_COLOR_BUFFER_BIT );
 
-        double newTime = TimeUtils.millis () / 1000.0;
-        double frameTime = Math.min ( newTime - currentTime, 0.25f );
-
-        currentTime = newTime;
+        // TODO: If time permits, fix this time step function.
+        //double newTime = TimeUtils.millis () / 1000.0;
+        //double frameTime = Math.min ( newTime - currentTime, 0.25f );
+        //double currentTime;
+        //currentTime = newTime;
         accumulator += delta;
         while ( accumulator >= step ) {
-            gameWorld.step ( step, 6, 2 );
             accumulator -= step;
             pHandler.updatePlatforms ();
             game_stage.act ( delta );
@@ -89,15 +70,13 @@ public class GameScreen extends AbstractScreen
 
     @Override public void show ()
     {
-        GamePreferences gprefs = new GamePreferences ();
         game_stage = new Stage ();
-        camera = new OrthographicCamera ( Physics.toMeters ( gprefs.getWidthResolution () ),
-                                          Physics.toMeters ( gprefs.getHeightResolution () ) );
+        camera = new OrthographicCamera ();
 
-        pHandler.setPlatforms ( 20, gameWorld );
-        platforms = pHandler.getPlatforms ();
-        for ( int ctr = 0; ctr < platforms.size (); ctr++ ) {
-            game_stage.addActor ( platforms.get( ctr ) );
+        pHandler.setPlatforms ( 20 );
+        ArrayList < Platform > platforms = pHandler.getPlatforms ();
+        for ( Platform p : platforms ) {
+            game_stage.addActor ( p );
         }
 
         game_stage.addActor ( player );
