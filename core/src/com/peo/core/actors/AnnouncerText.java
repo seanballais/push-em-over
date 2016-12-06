ab6deb34bc096028fc525a28b7c00475f318b5da
seanballais <sean@seanballais.com>
2016-12-06 15:38:04 +0800

    Added announcer text.

---
+++ core/src/com/peo/core/actors/AnnouncerText.java
@@ -0,0 +1,48 @@
+package com.peo.core.actors;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.peo.utils.GamePreferences;
+
+public class AnnouncerText extends Actor
+{
+    private Texture textSpritesheet;
+    private GamePreferences gamePreferences;
+    private TextureRegion [] frames;
+    private int frameNum;
+    private int timeDelta;
+
+    public AnnouncerText ()
+    {
+        textSpritesheet = new Texture ( Gdx.files.internal ( "img/actors/game-start.png" ) );
+        gamePreferences = new GamePreferences ();
+        frames = new TextureRegion [ 3 ];
+        frameNum = 0;
+        timeDelta = 0;
+
+        for ( int frameCount = 0; frameCount < frames.length; frameCount++ ) {
+            frames [ frameCount ] = new TextureRegion (
+                    textSpritesheet, 0, frameCount * 130, textSpritesheet.getWidth (), 130
+            );
+        }
+    }
+
+    @Override public void draw ( Batch batch, float parentAlpha )
+    {
+        if ( timeDelta == 1000 ) {
+            timeDelta = 0;
+            frameNum += 1;
+        }
+
+        timeDelta++;
+
+        batch.draw (
+            frames [ frameNum ],
+            ( gamePreferences.getWidthResolution () / 2 ) - ( textSpritesheet.getWidth () / 2 ),
+            gamePreferences.getHeightResolution () / 2
+        );
+    }
+}
